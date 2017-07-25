c4b4795ca3ec42c7a3fc213a9a25f5b4a7dcdecb
Lucas Reynolds <lreynolds18@gmail.com>
2017-07-25 15:25:41 -0400

    testing react ga

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,16 @@ import About from './About';
 import Work from './Work';
 import Projects from './Projects';
 import { Grid, Row, Col, Navbar, Nav, NavItem } from 'react-bootstrap';
+import ReactGA from 'react-ga';
 
 import s from '../styles/app.style';
+
+ReactGA.initialize('UA-103151343-1');
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 export default function App() {
 
@@ -42,11 +50,11 @@ export default function App() {
           <Col xs={1} md={1}></Col>
           <Col xs={9} md={9}>
             <Switch>
-              <Route exact path="/" component={About} />
-              <Route exact path="/about" component={About} />
-              <Route path="/work-experience" component={Work} />
-              <Route path="/projects" component={Projects} />
-              <Route component={PageNotFound} />
+              <Route exact path="/" component={About} onUpdate={logPageView} />
+              <Route exact path="/about" component={About} onUpdate={logPageView} />
+              <Route path="/work-experience" component={Work} onUpdate={logPageView} />
+              <Route path="/projects" component={Projects} onUpdate={logPageView} />
+              <Route component={PageNotFound} onUpdate={logPageView} />
             </Switch>
           </Col>
           <Col xs={1} md={1}></Col>
