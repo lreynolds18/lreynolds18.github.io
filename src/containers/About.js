23dfb2bb5b85a7a6a45367ef3130609109c207c8
Lucas Reynolds <lreynolds18@gmail.com>
2018-01-13 17:05:17 -0500

    file cleanup: moved changelog to it's own file

--- src/containers/About.js
+++ src/containers/About.js
@@ -6,30 +6,13 @@ import { Link } from 'react-router-dom';
 import { Table, Grid, Row, Col, Button, ButtonToolbar } from 'react-bootstrap';
 import { FaGithubAlt, FaLinkedinSquare, FaEnvelope } from 'react-icons/lib/fa';
 
+import Changelog from '../components/Changelog';
+
 
 export default function About() {
     const header = "I am a developer from Southeast Michigan.  I'm passionate about anything related to CS (compilers, assembly code <3), but I am especially passionate about data science (including machine learning, deep learning, reinforcement learning, statistics and mathematics) and robotics.  In my free time, I enjoy the outdoors, learning French and cooking.";
         
     const header2 = "Find me around the web at lreynolds18";
-    const data = [{date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {date: "12/2/18", text: "Connected to facebook", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},
-                  {url: "www.google.com", date: "11/2/13", text: "Connected to google", importance: "low"},];
-    const all = true;
 
     return (
         <div>
@@ -63,38 +46,7 @@ export default function About() {
                 <br />
                 <Row>
                     <Col xs={10} md={10}>
-                        <h4>Changelog</h4>
-                        <Table bordered condensed hover style={{"border" : "0.5px solid #000000"}}>
-                            <thead style={{"border" : "0.5px solid #000000"}}>
-                                <tr style={{"border" : "0.5px solid #000000"}}>
-                                    <th style={{"border" : "0.5px solid #000000"}}>Date</th>
-                                    <th style={{"border" : "0.5px solid #000000"}}>Event</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.reverse().map(function(item, idx) {
-                                    if (idx < 10 || all) {
-                                        if (item.url) {
-                                            return <tr key={idx} style={{"border" : "0.5px solid #000000"}}>
-                                                     <td style={{"border" : "0.5px solid #000000"}}><a href={item.url}>{item.date}</a></td>
-                                                     <td style={{"border" : "0.5px solid #000000"}}><a href={item.url}>{item.text}</a></td>
-                                                   </tr>;
-                                        } else {
-                                            return <tr key={idx} style={{"border" : "0.5px solid #000000"}}>
-                                                     <td style={{"border" : "0.5px solid #000000"}}>{item.date}</td>
-                                                     <td style={{"border" : "0.5px solid #000000"}}>{item.text}</td>
-                                                   </tr>;
-                                        }
-                                    }
-                                })}
-                            </tbody>
-                        </Table>
-                        <ButtonToolbar>
-                            <Button
-                              bsStyle="primary"
-                              href="/about/?all=true"
-                            >Show All</Button>
-                        </ButtonToolbar>
+                        <Changelog />
                     </Col>
                 </Row>
                 <br />
