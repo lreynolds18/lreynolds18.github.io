001418652a18593c3656fdcfe9a99f7a049ed155
Lucas Reynolds <lreynolds18@gmail.com>
2018-01-16 13:32:39 -0500

    idc about jsx file.  removing regex to look for jsx

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
   },
 
