1c79c02c6c164d9858bda1fdef4f7cff03f0b10b
Lucas Reynolds <lreynolds18@gmail.com>
2019-04-07 19:50:23 -0400

    fixed projects page -- still need to style

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import { Container, Grid, Image } from 'semantic-ui-react';
+import ReactPlayer from 'react-player';
 
-// import { Card } from 'semantic-ui-react';
-// import Iframe from 'react-iframe';
 import resource from '../resource';
 
 export default class Projects extends Component {
+  renderSingleProject(project, index) {
+    return (
+      <Grid columns={1} key={index}>
+        <Grid.Row>
+          <h2>{ project.title }</h2>
+          <h4>{ project.date }</h4>
+        </Grid.Row>
+
+        <Grid.Row>
+          <h3>{ project.stack }</h3>
+        </Grid.Row>
+
+        {project.player.map((vid, idx) =>
+          <Grid.Row key={idx}>
+            <ReactPlayer url={vid} controls />
+          </Grid.Row>
+        )}
+
+        <Grid.Row>
+          {project.overview.map((line, idx) =>
+            <p key={idx}>{ line }</p>
+          )}
+        </Grid.Row>
+
+        {project.images.map((image, idx) =>
+          <Grid.Row key={idx}>
+            <Image src={image.url} />
+            <p>{ image.label }</p>
+          </Grid.Row>
+        )}
+      </Grid>
+    );
+  }
+
   render() {
     return (
-      <Container>      
+      <Container>
         <h1>{ resource.Projects.Title }</h1>
+
+        {resource.Projects.Projects.map((project, idx) =>
+          { return this.renderSingleProject(project, idx) }
+        )}
       </Container>      
     );
   }
